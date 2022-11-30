9a94f3b4f2514018c4d3ecc24cabec5b5c018033
DeepakBarwal <deepakbarwal98@gmail.com>
2022-11-30 16:16:40 +0530

    added getNotes function to get back notes from backend. Also performed some cleanup.

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,24 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
 
 function App() {
+  const getNotes = async () => {
+    try {
+      const response = await axios.get(
+        `http://localhost:5000/notes`
+      );
+      console.log(response)
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
   return (
     <div className="App">
       <div>Notes Application</div>
+      <div>
+        <button onClick={getNotes}>Click Me</button>
+      </div>
     </div>
   );
 }
