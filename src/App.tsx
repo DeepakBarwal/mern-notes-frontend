ad4584b149c3b1471e0d78d7f884c112025b3bb3
DeepakBarwal <deepakbarwal98@gmail.com>
2022-11-30 16:49:34 +0530

    created state notesList and setting it instead of logging to console

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,16 @@
 import './App.css';
 import axios from 'axios';
+import { useState } from 'react';
 
 function App() {
+  const [notesList, setNotesList] = useState([]);
+
   const getNotes = async () => {
     try {
       const response = await axios.get(
         `http://localhost:5000/notes`
       );
-      console.log(response)
+      setNotesList(response.data.notes);
     } catch (error) {
       console.error(error);
     }
