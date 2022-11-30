7f42d6aee294f17139af8b1614e0b837d48a40a0
DeepakBarwal <deepakbarwal98@gmail.com>
2022-11-30 17:07:10 +0530

    created DUMMY_NOTES.ts

---
+++ src/DUMMY_NOTES.ts
@@ -0,0 +1,16 @@
+import INote from "./interfaces/note.interface";
+
+const DUMMY_NOTES: INote[] = [
+  { text: "Do thing #1", link: "https://thing1.com" },
+  { text: "Do thing #2", link: "https://thing2.com" },
+  { text: "Do thing #3", link: "https://thing3.com" },
+  { text: "Do thing #4", link: "https://thing4.com" },
+  { text: "Do thing #5", link: "https://thing5.com" },
+  { text: "Do thing #6", link: "https://thing6.com" },
+  { text: "Do thing #7", link: "https://thing7.com" },
+  { text: "Do thing #8", link: "https://thing8.com" },
+  { text: "Do thing #9", link: "https://thing9.com" },
+  { text: "Do thing #10", link: "https://thing10.com" },
+];
+
+export default DUMMY_NOTES;
