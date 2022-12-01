1b96d904fc4b7705871ffd1de7dc706edf4f71b8
DeepakBarwal <deepakbarwal98@gmail.com>
2022-12-01 15:02:59 +0530

    added attribute suppressContentEditableWarning to note__text div

--- src/components/Note/Note.tsx
+++ src/components/Note/Note.tsx
@@ -10,7 +10,9 @@ type Props = {
 const Note: FC<Props> = ({ note }) => {
   return (
     <div className='note'>
-      <div className='note__text' contentEditable={true} >{note.text}</div>
+      <div className='note__text' contentEditable={true} suppressContentEditableWarning={true}>
+        {note.text}
+      </div>
       <div className='note__link'>
         <a href={note.link}>{note.link}</a>
       </div>
