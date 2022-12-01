38fef765fefbe883d76416578ef50ce6a9911515
DeepakBarwal <deepakbarwal98@gmail.com>
2022-12-01 15:17:06 +0530

    Note.tsx - added empty event handler noteTextUpdated onBlur of note__text

--- src/components/Note/Note.tsx
+++ src/components/Note/Note.tsx
@@ -1,16 +1,25 @@
-import { FC } from 'react';
+import { FC, FocusEvent } from 'react';
 import INote from '../../interfaces/note.interface';
 import './Note.css';
 
 type Props = {
   note: INote
+};
+
+const noteTextUpdated = (event: FocusEvent<HTMLDivElement>) => {
+  console.log(event.currentTarget.textContent);
 }
 
 // Dumb, presentational component
 const Note: FC<Props> = ({ note }) => {
   return (
     <div className='note'>
-      <div className='note__text' contentEditable={true} suppressContentEditableWarning={true}>
+      <div 
+        className='note__text' 
+        contentEditable={true} 
+        suppressContentEditableWarning={true}
+        onBlur={noteTextUpdated}
+      >
         {note.text}
       </div>
       <div className='note__link'>
