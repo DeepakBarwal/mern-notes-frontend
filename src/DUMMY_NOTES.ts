455bf82f9bf0b01ba855d0527532600ec97afd60
DeepakBarwal <deepakbarwal98@gmail.com>
2022-12-01 15:53:57 +0530

    added _id to note.interface.ts and to DUMMY_NOTES.ts

--- src/DUMMY_NOTES.ts
+++ src/DUMMY_NOTES.ts
@@ -1,16 +1,16 @@
 import INote from "./interfaces/note.interface";
 
 const DUMMY_NOTES: INote[] = [
-  { text: "Do thing #1", link: "https://thing1.com" },
-  { text: "Do thing #2", link: "https://thing2.com" },
-  { text: "Do thing #3", link: "https://thing3.com" },
-  { text: "Do thing #4", link: "https://thing4.com" },
-  { text: "Do thing #5", link: "https://thing5.com" },
-  { text: "Do thing #6", link: "https://thing6.com" },
-  { text: "Do thing #7", link: "https://thing7.com" },
-  { text: "Do thing #8", link: "https://thing8.com" },
-  { text: "Do thing #9", link: "https://thing9.com" },
-  { text: "Do thing #10", link: "https://thing10.com" },
+  { _id: '1', text: "Do thing #1", link: "https://thing1.com" },
+  { _id: '2', text: "Do thing #2", link: "https://thing2.com" },
+  { _id: '3', text: "Do thing #3", link: "https://thing3.com" },
+  { _id: '4', text: "Do thing #4", link: "https://thing4.com" },
+  { _id: '5', text: "Do thing #5", link: "https://thing5.com" },
+  { _id: '6', text: "Do thing #6", link: "https://thing6.com" },
+  { _id: '7', text: "Do thing #7", link: "https://thing7.com" },
+  { _id: '8', text: "Do thing #8", link: "https://thing8.com" },
+  { _id: '9', text: "Do thing #9", link: "https://thing9.com" },
+  { _id: '10', text: "Do thing #10", link: "https://thing10.com" },
 ];
 
 export default DUMMY_NOTES;
