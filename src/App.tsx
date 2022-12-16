4e79d23904ba8e6931db18126d5b45fd3035bb20
DeepakBarwal <DeepakBarwal98@gmail.com>
2022-12-16 13:25:30 +0530

    added modal to add note

--- src/App.tsx
+++ src/App.tsx
@@ -5,9 +5,15 @@ import DUMMY_NOTES from './DUMMY_NOTES';
 import Note from './components/Note/Note';
 import INote from './interfaces/note.interface';
 import { getNotes } from './components/services/notesService';
+import { Button, Modal } from 'react-bootstrap';
 
 function App() {
   const [notesList, setNotesList] = useState<Array<INote>>(JSON.parse(localStorage.getItem('my-notes') || JSON.stringify(DUMMY_NOTES)));
+
+  const [showAddNoteModal, setShowAddNoteModal] = useState(false);
+
+  const handleCloseAddNoteModal = () => setShowAddNoteModal(false);
+  const handleShowAddNoteModal = () => setShowAddNoteModal(true);
 
   const saveNotesToLocalStorage = () => {
     const notesListString = JSON.stringify(notesList);
@@ -42,6 +48,24 @@ function App() {
 
   return (
     <div className="App">
+      <Button variant="primary" onClick={handleShowAddNoteModal}>
+        Launch demo modal
+      </Button>
+
+      <Modal show={showAddNoteModal} onHide={handleCloseAddNoteModal}>
+        <Modal.Header closeButton>
+          <Modal.Title>Modal heading</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>Woohoo, you're reading this text in a modal!</Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleCloseAddNoteModal}>
+            Close
+          </Button>
+          <Button variant="primary" onClick={handleCloseAddNoteModal}>
+            Save Changes
+          </Button>
+        </Modal.Footer>
+      </Modal>
       <div className="notes-list">
         {
           notesList.map((noteItem, index) => {
